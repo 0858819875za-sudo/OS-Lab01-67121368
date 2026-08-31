493ccb539c4549a1a85c7fc11b9790b068544f40
0858819875za-sudo <0858819875za@gmail.com>
2026-08-31 11:54:53 +0000

    Completed Lab 1: System Profiling

---
+++ profiler.py
@@ -0,0 +1,9 @@
+import os
+import platform
+import psutil
+
+print(f"OS Name: {platform.system()} {platform.release()}")
+print(f"Number of CPU Cores: {psutil.cpu_count(logical=True)}")
+print(
+    f"Total RAM: {round(psutil.virtual_memory().total / (1024**3), 2)} GB"
+)
---
+++ stress_test.py
@@ -0,0 +1,6 @@
+import math
+
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+  math.factorial(50000)
+
